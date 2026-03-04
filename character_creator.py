148c3336b9c24a1df8b84a2707d16e1f05b082fb
faanshavohra-coder <faanshavohra@gmail.com>
2026-03-05 00:30:38 +0300

    Update charactor_creator.py

--- character_creator.py
+++ character_creator.py
@@ -1,9 +1,9 @@
 def create_character(character_name, strength, intelligence, charisma):
-    # 1. Define the symbols inside the function (important for some tests)
+    
     full_dot = "●"
     empty_dot = "○"
 
-    # 2. Error checks with EXACT punctuation (periods at the end!)f not isinstance(character_name,str):
+    
     if not isinstance(character_name,str):
         return "The character name should be a string"
     if not character_name:
@@ -24,12 +24,13 @@ def create_character(character_name, strength, intelligence, charisma):
     if (strength + intelligence + charisma) != 7:
         return "The character should start with 7 points"
 
-    # 3. Create the lines (Check the space after STR, INT, CHA)
+     
     line_1 = character_name
     line_2 = "STR " + (full_dot * strength) + (empty_dot * (10 - strength))
     line_3 = "INT " + (full_dot * intelligence) + (empty_dot * (10 - intelligence))
     line_4 = "CHA " + (full_dot * charisma) + (empty_dot * (10 - charisma))
 
-    # 4. Return as a single string with newlines
+    
     return f"{line_1}\n{line_2}\n{line_3}\n{line_4}"
+
   
