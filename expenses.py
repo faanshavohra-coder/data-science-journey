4fbdb0a0721f2a288852b2b1e25c2398079124de
faanshavohra-coder <faanshavohra@gmail.com>
2026-03-05 17:35:56 +0300

    Update expenses.py

--- expenses.py
+++ expenses.py
@@ -3,7 +3,8 @@ monthly_expenses = {"Rent":1200,
                     "Groceries":400,
                     "Internet":60,
                     "Gym":50,
-                    "Streaming":30}
+                    "Streaming":30,
+                    "Cofffe": 40}
 # 2. A function to calculate the total and check the budget
 def analyze_spending (expenses_dict,budget_limit):
     total = sum(expenses_dict.values())
@@ -13,5 +14,5 @@ def analyze_spending (expenses_dict,budget_limit):
     else:
         print("Looking good!You are within budget.")
         return total
-# 3. Running the function with a $1500 limit
-analyze_spending(monthly_expenses,1500)
+
+
